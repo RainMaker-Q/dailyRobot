08cb886aa1541934d2e58aa3d0cdc1616e90ac4d
LRQ <aa651577562@qq.com>
2019-12-21 22:55:54 +0800

    修改配置

---
+++ config.js
@@ -0,0 +1,19 @@
+
+var configList = {
+  lrq: {
+    authCode: "server酱的授权码",
+    province: "beijing",
+    city: "haidian-district"
+  },
+  lxq: {
+    authCode: "123",
+    province: "heilongjiang",
+    city: "harbin"
+  }
+}
+
+
+
+module.exports = {
+  configList: configList
+}
