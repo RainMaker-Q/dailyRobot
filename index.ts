9d488a39de51300ce0bd3de4344fb658a1832959
rainqingliu <liuruiqing@kuaishou.com>
2022-06-19 21:24:38 +0800

    feat: update words 2 mysql

--- index.ts
+++ index.ts
@@ -2,7 +2,11 @@ const schedule = require("node-schedule");
 
 import configList from "./config";
 import { sendServerChan } from "./utils/send";
-import { getAllMessage, getCustomizedMessage } from "./utils/messege";
+import {
+  getAllMessage,
+  getCustomizedMessage,
+  updateTodayWords,
+} from "./utils/messege";
 
 const send = async function () {
   await getAllMessage();
@@ -10,15 +14,16 @@ const send = async function () {
     const messege = getCustomizedMessage(person);
     await sendServerChan(person, messege);
   }
+  updateTodayWords();
 };
 
 /*
  *定时任务
  *秒 分 时 日 月 周
  */
-schedule.scheduleJob("00 20 05 * * *", async () => {
-  await send();
-  console.log("======信息已发送======" + new Date());
-});
+// schedule.scheduleJob("00 20 05 * * *", async () => {
+//   await send();
+//   console.log("======信息已发送======" + new Date());
+// });
 
-// send();
+send();
--- utils/messege.ts
+++ utils/messege.ts
@@ -4,6 +4,7 @@ import getYdWord from "../apis/getYdWord";
 
 import { IMessege, Store } from "../types/index";
 import configList, { IConfig } from "../config";
+import axios from "axios";
 
 const MessegeStore = {} as Store;
 
@@ -37,4 +38,18 @@ export const getCustomizedMessage = function (config: IConfig) {
   messege["weather"] = MessegeStore[key];
   messege["extraWord"] = extraWord;
   return messege;
+};
+
+export const updateTodayWords = () => {
+  axios
+    .post("http://localhost:3000/words", {
+      yd_ch: MessegeStore?.ydWord?.chinese || "",
+      yd_en: MessegeStore?.ydWord?.english || "",
+      yd_img: MessegeStore?.ydWord?.image || "",
+      yd_mp3: MessegeStore?.ydWord?.mp3 || "",
+      theone: MessegeStore?.one?.text || "",
+    })
+    .then((res) => {
+      console.log("res is ", res.data);
+    });
 };
